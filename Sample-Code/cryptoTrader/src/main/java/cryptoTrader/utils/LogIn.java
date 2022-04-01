66269096dec06003920a5f04c3e9253b5467d719
Karen <73141531+karenzhan8@users.noreply.github.com>
2022-04-01 18:12:53 -0400

    log-in class to confirm user credentials

---
+++ Sample-Code/cryptoTrader/src/main/java/cryptoTrader/utils/LogIn.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class LogIn {
+	public boolean verify (String user, String pass) {
+		try {
+			File file = new File("login.txt");
+			Scanner input = new Scanner(file);
+			String[] curr = {"hello", "world"};
+			
+			while (input.hasNextLine()) {
+				curr = input.nextLine().split(" ");
+				
+				if (curr[0].equals(user) && curr[1].equals(pass)) {
+					return true;
+				}
+			}
+			return false;	
+		} 
+		catch (FileNotFoundException e) {
+			return false;
+		}
+	}	
+}
