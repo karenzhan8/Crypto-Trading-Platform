5ec71c788319ec6ca7197b41775f126b97d49530
karenzhan8 <karenzhan18@gmail.com>
2022-04-01 17:09:21 -0400

    created login file, started work on LogIn class

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,1 +1,27 @@
 
+
+public class LogIn {
+	
+	public boolean verify (String user, String pass) {
+		File file = new File("login.txt");
+		Scanner input = new Scanner (file);
+		
+		String[] curr = {"", ""};
+		String currUser = "";
+		String currPass = "";
+		
+		while (scanner.hasNextLine()) {
+			curr = scanner.next().split(" ");
+			if (curr[0].equals(user) && curr[1].equals(pass)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static void main (String[] args) {
+		System.out.println(verify());
+	}
+	
+}
