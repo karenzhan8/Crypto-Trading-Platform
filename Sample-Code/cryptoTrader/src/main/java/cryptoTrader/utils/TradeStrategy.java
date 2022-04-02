a6c90e295d2a452dc334265ecc883d34a3b0cc3e
zoede <zoedesouzasc@gmail.com>
2022-04-01 21:03:39 -0400

    added tradestrategy pseudocode

---
+++ Sample-Code/cryptoTrader/src/main/java/cryptoTrader/utils/TradeStrategy.java
@@ -0,0 +1,15 @@
+// the trade strategy class
+
+// takes string as input
+
+// chooses strategy and executes it, conditions for trade must be selected coins
+// but coin to buy/sell does not have to be
+
+
+//A: if BTC > 46k and ETH < 3800 then buy 800 units Cardano, else sell 2 Bitcoin
+
+//B: if Cardano > $1 then buy 10 LUNA, else sell 3 Bitcoin
+
+//C: if ethereum > 3800 and Cardano <$1 buy 200 FTM, else sell 500 Cardano
+
+//D: if price of Cardano > price FTM, then buy 100 FTM, else buy 100 Cardano
