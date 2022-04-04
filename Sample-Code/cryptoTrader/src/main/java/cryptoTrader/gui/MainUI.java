8bfa24d6437668c789dbbb4c1ff3ac36d67f9a72
rarmena2 <84240274+rarmena2@users.noreply.github.com>
2022-04-04 11:14:05 -0400

    Update MainUI.java

--- Sample-Code/cryptoTrader/src/main/java/cryptoTrader/gui/MainUI.java
+++ Sample-Code/cryptoTrader/src/main/java/cryptoTrader/gui/MainUI.java
@@ -19,10 +19,8 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
@@ -30,15 +28,13 @@ import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-import utils.DataVisualizationCreator;
-import utils.ExecuteTrade;
-import utils.LogIn;
-import utils.UserSelection;
+import cryptoTrader.utils.AvailableCryptoList;
+import cryptoTrader.utils.DataVisualizationCreator;
+import cryptoTrader.utils.ExecuteTrade;
+import cryptoTrader.utils.UserSelection;
 
 import javax.swing.JTextField;
 import javax.swing.JLabel;
-
-import utils.DataVisualizationCreator;
 
 public class MainUI extends JFrame implements ActionListener {
 	/**
@@ -63,21 +59,12 @@ public class MainUI extends JFrame implements ActionListener {
 	private String selectedStrategy = "";
 	private DefaultTableModel dtm;
 	private JTable table;
-	private JTextField Name;
-	private JTextField Coin;
-	private JTextField Strategy;
-	
-	private static JFrame frame = new JFrame();
-	private static JFrame loginFrame = new JFrame();
-	private static JLabel passwordLabel, label;
-	private static JTextField username;
-	private static JPasswordField password;
-	private static JButton button;
 	
 	// stores database of brokers
 	UserSelection brokerDatabase = new UserSelection();
 	// stores list of cumulative trades
 	ExecuteTrade cumulativeTrades = new ExecuteTrade();
+	
 
 	public static MainUI getInstance() {
 		if (instance == null)
@@ -93,41 +80,8 @@ public class MainUI extends JFrame implements ActionListener {
 
 		// Set top bar
 
+
 		JPanel north = new JPanel();
-		
-		//facilitate log in and verifying credentials
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		
-		loginFrame.setTitle("Login");
-		loginFrame.add(panel);
-		loginFrame.setSize(new Dimension (400, 200));
-		
-		label = new JLabel ("Username");
-		label.setBounds(100, 8, 70, 20);
-		panel.add(label);
-		
-		username = new JTextField();
-		username.setBounds(100, 27, 193, 28);;
-		panel.add(username);
-		
-		passwordLabel = new JLabel("Password");
-		passwordLabel.setBounds(100, 55, 70, 20);
-		panel.add(passwordLabel);
-		
-		password = new JPasswordField();
-		password.setBounds(100, 75, 193, 28);
-		panel.add(password);
-		
-		button = new JButton("Login");
-		button.setBounds(100, 110, 90, 25);
-		button.addActionListener(this);
-		panel.add(button);
-		
-		loginFrame.setLocationRelativeTo(null);
-		loginFrame.setVisible(true);
-		loginFrame.setAlwaysOnTop(true);
-	
 //
 //		north.add(strategyList);
 //
@@ -165,11 +119,13 @@ public class MainUI extends JFrame implements ActionListener {
 		trade.setActionCommand("refresh");
 		trade.addActionListener(this);
 
+
+
 		JPanel south = new JPanel();
 		
 		south.add(trade);
 
-		dtm = new DefaultTableModel(new Object[] { "Trading Client", "Coin List", "Strategy Name" }, 0);
+		dtm = new DefaultTableModel(new Object[] { "Trading Client", "Coin List", "Strategy Name" }, 1);
 		table = new JTable(dtm);
 		// table.setPreferredSize(new Dimension(600, 300));
 		JScrollPane scrollPane = new JScrollPane(table);
@@ -204,25 +160,7 @@ public class MainUI extends JFrame implements ActionListener {
 		JLabel lblNewLabel = new JLabel("Enter Broker Name:");
 		east.add(lblNewLabel);
 		
-		Name = new JTextField();
-		east.add(Name);
-		Name.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("Enter Coin(s):");
-		east.add(lblNewLabel_1);
-		
-		Coin = new JTextField();
-		east.add(Coin);
-		Coin.setColumns(10);
-		
-		JLabel lblNewLabel_2 = new JLabel("Enter Strategy:");
-		east.add(lblNewLabel_2);
-		
-		Strategy = new JTextField();
-		east.add(Strategy);
-		Strategy.setColumns(10);
-		Strategy.setText("Strategy-");
-		
+	
 		JPanel buttons = new JPanel();
 		buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
 		buttons.add(addRow);
@@ -252,7 +190,8 @@ public class MainUI extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-		frame = MainUI.getInstance();
+		
+		JFrame frame = MainUI.getInstance();
 		frame.setSize(900, 600);
 		frame.pack();
 		frame.setVisible(true);
@@ -267,8 +206,14 @@ public class MainUI extends JFrame implements ActionListener {
 					if (traderObject == null) {
 						JOptionPane.showMessageDialog(this, "please fill in Trader name on line " + (count + 1) );
 						return;
-					} // CHECK HERE IF NAME MATCHES A PREVIOUS BROKER NAME
-					String traderName = traderObject.toString(); // traderName string
+					}
+					String traderName = traderObject.toString();
+					/*
+					if (brokerDatabase.inDatabase(traderName) == null) {
+						JOptionPane.showMessageDialog(this, "please change your Trader name on line " + (count + 1) );
+						return;// CHECK HERE IF NAME MATCHES A PREVIOUS BROKER NAME
+					};
+					*/
 					Object coinObject = dtm.getValueAt(count, 1); 
 					if (coinObject == null) {
 						JOptionPane.showMessageDialog(this, "please fill in cryptocoin list on line " + (count + 1) );
@@ -279,41 +224,56 @@ public class MainUI extends JFrame implements ActionListener {
 					if (strategyObject == null) {
 						JOptionPane.showMessageDialog(this, "please fill in strategy name on line " + (count + 1) );
 						return;
+					} else {
+							String ticker = AvailableCryptoList.getInstance().coinAvailable(coinNames);
+						if (ticker != null) {
+							JOptionPane.showMessageDialog(this, "the following coin is not valid: " + ticker + " on line " + (count + 1) );
+							return;
+						}
 					}
 					String strategyName = strategyObject.toString(); // strategy name string
 					System.out.println(traderName + " " + Arrays.toString(coinNames) + " " + strategyName); // could use here to make selection object
-	        }
+					brokerDatabase.addBroker(traderName, strategyName, coinNames);
+			}
+			
 			cumulativeTrades.performTrade(brokerDatabase);
+			
 			stats.removeAll();
 			DataVisualizationCreator creator = new DataVisualizationCreator();
-			//creator.createCharts(cumulativeTrades.getCumulativeTrades(), histoList);
+		// ---------------- TESTING PURPOSES ONLY (TO DELETE FOR FINAL PRODUCT) -------------------------
+			List<List<String>> tableList = new ArrayList<List<String>>();
+			List<String> tradeData = new ArrayList<String>();
+			tradeData.add("taylor");
+			tradeData.add("strategy-a");
+			tradeData.add("buy");
+			tradeData.add("BTC");
+			tradeData.add("300");
+			tradeData.add("1.00");
+			tradeData.add("03-06-2002");
+			tableList.add(tradeData);
+			
+			List<List<String>> histoList = new ArrayList<List<String>>();
+			List<String> freqData = new ArrayList<String>();
+			freqData.add("6");
+			freqData.add("Trader-1");
+			freqData.add("Strategy-A");
+			histoList.add(freqData);
+			
+			List<String> freqData2 = new ArrayList<String>();
+			freqData2.add("5");
+			freqData2.add("Trader-2");
+			freqData2.add("Strategy-B");
+			histoList.add(freqData2);
+			
+			creator.createCharts(cumulativeTrades.getCumulativeTrades(), histoList);
+		// ---------------------------------------------------------------
+ 		//	creator.createCharts(cumulativeTrades.getCumulativeTrades());
 		} else if ("addTableRow".equals(command)) {
-			String[] coinList = Coin.getText().split(",");
-			String[] newRow = {Name.getText(), Coin.getText(), Strategy.getText()};
-			brokerDatabase.addBroker(newRow[0], newRow[2], coinList);
-			dtm.addRow(newRow); 
+			dtm.addRow(new String[3]); 
 		} else if ("remTableRow".equals(command)) {
 			int selectedRow = table.getSelectedRow();
 			if (selectedRow != -1) {
 				dtm.removeRow(selectedRow); // connect to remove the selected row data from here (THIS IS PERFORMED AFTER TRADE BUTTON HIT)
-			}
-		} else if ("Login".equals(command)) {
-			//verify that user credentials are correct
-			LogIn database = new LogIn();
-			
-			String user = username.getText();
-			String pass = password.getText();
-			
-			//if credentials valid, hide log in panel from user
-			if (database.verify(user, pass)) {
-				loginFrame.setVisible(false);
-			}
-			
-			//if credentials invalid, close system and exit system
-			else {
-				frame.setVisible(false);
-				loginFrame.setVisible(false);
-				System.exit(1);
 			}
 		}
 	}
